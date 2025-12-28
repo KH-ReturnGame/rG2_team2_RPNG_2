64a41b1271dc6ef72a9be01232398803e911bf08
Lmotorness <jhw0222@iCloud.com>
2025-12-28 23:24:58 +0900

    commit1

--- Assets/Scripts/Player/PlayerXP.cs
+++ Assets/Scripts/Player/PlayerXP.cs
@@ -2,21 +2,32 @@ using UnityEngine;
 
 public class PlayerXP : MonoBehaviour
 {
+    public static PlayerXP Instance;
+
     public int currentLevel = 1;
     public int currentXp = 0;
-
-    // You can set this in the Inspector or populate it in Awake()
     public int[] xpCap = { 0, 10, 25, 50, 100, 200, 400 }; 
-    // index = level number, value = xp required to reach that level
+
+    void Awake()
+    {
+        // Singleton pattern (only one PlayerXP allowed)
+        if (Instance != null && Instance != this)
+        {
+            Destroy(gameObject);
+            return;
+        }
+
+        Instance = this;
+        DontDestroyOnLoad(gameObject);
+    }
 
     public void AddXp(int amount)
     {
         currentXp += amount;
 
-        // Check if we passed the cap for the current level
-        while (currentLevel < xpCap.Length - 1 && currentXp >= xpCap[currentLevel])
+        while (currentLevel < xpCap.Length && currentXp >= xpCap[currentLevel])
         {
-            currentXp -= xpCap[currentLevel]; // carry over extra XP
+            currentXp -= xpCap[currentLevel];
             currentLevel++;
             LevelUp();
         }
@@ -24,11 +35,8 @@ public class PlayerXP : MonoBehaviour
 
     void LevelUp()
     {
-        Debug.Log("Level Up! Reached Level: " + currentLevel);
-
-        // Do something on level up:
-        // - Increase stats
-        // - Unlock abilities
-        // - Play animation
+        Debug.Log("Level Up! Level: " + currentLevel);
     }
 }
+
+
